cd3925f7eb7024d502939bd1434db0eab1e01ce6
chiranwijesekara <chiranwijesekara18@gmail.com>
2025-07-11 12:55:39 +0530

    Initial commit without node_modules

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='footer'>
-        <div className='container mx-auto flex-col md:flex-row justify-between items-center'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
             <div className='w-full md:w-1/3 mb-8 md:mb-0'>
                 <img src={assets.logo_dark}></img>
-                <p className='text gray-400 mt-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text.</p>
+                <p className='text-gray-400 mt-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text.</p>
 
             </div>
             <div className='w-full md:w-1/5 mb-8 md:mb-0'>
@@ -28,7 +28,7 @@ const Footer = () => {
                  </div>
             </div>
         </div>
-        <div>Copyright 2025  &copy; CP Wijesekara. All Right Reserved.</div>
+        <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>Copyright 2025  &copy; CP Wijesekara. All Right Reserved.</div>
     </div>
   )
 }
